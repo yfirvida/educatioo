65406dd2640fab441a15e7b90ceb8cae8e841879
yfirvida <yfirvida@yahoo.es>
2022-01-17 10:26:48 -0500

    Trainer Navigation Menu keep active btn

--- resources/views/includes/trainer-menu.blade.php
+++ resources/views/includes/trainer-menu.blade.php
@@ -1,27 +1,27 @@
 <nav class="sidebar" id="sidebar">
   <div class="side-wrap">
-    <a class="menu-btn" href="{{ route('classrooms') }}">
+    <a class="menu-btn {{request()->routeIs('classrooms') ? 'active' : '' }}" href="{{ route('classrooms') }}">
       <div class="text-center">
         <img class="normal" src="/img/classroom.png">
         <img class="active" src="/img/classroom_active.png">
         <span class="menu-title">{{ __('Course Group') }}</span>
       </div>
     </a>
-    <a class="menu-btn" href="{{ route('courses') }}">
+    <a class="menu-btn {{request()->routeIs('courses') ? 'active' : '' }}" href="{{ route('courses') }}">
       <div class="text-center">
         <img class="normal" src="/img/course.png">
         <img class="active" src="/img/course_active.png">
         <span class="menu-title">{{ __('Courses') }}</span>
       </div>
     </a>
-    <a class="menu-btn" href="{{ route('results') }}">
+    <a class="menu-btn {{request()->routeIs('results') ? 'active' : '' }}" href="{{ route('results') }}">
       <div class="text-center">
         <img class="normal" src="/img/result.png">
         <img class="active" src="/img/result_active.png">
         <span class="menu-title">{{ __('Results') }}</span>
       </div>
     </a>
-    <a class="menu-btn" href="{{ route('launch') }}">
+    <a class="menu-btn {{request()->routeIs('launch') ? 'active' : '' }}" href="{{ route('launch') }}">
       <div class="text-center">
         <img class="normal" src="/img/launch.png">
         <img class="active" src="/img/launch_active.png">
