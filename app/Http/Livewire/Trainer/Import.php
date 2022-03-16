839855213411fd4513e07581268bab3b25c33483
yfirvida <yfirvida@yahoo.es>
2022-03-15 23:51:36 -0400

    trainer Import (validate url, fix error message in view)

--- app/Http/Livewire/Trainer/Import.php
+++ app/Http/Livewire/Trainer/Import.php
@@ -18,46 +18,41 @@ class Import extends Component
     {
         $validatedData = $this->validate([
             'name' => 'required',
-            'url' => 'required'
+            'url' => 'required|url'
 
         ]);
 
         $parts = parse_url($this->url);
-        parse_str($parts['query'], $query);
-        $course_id = $query['exam'];
-        $user = Auth::user();
+        if (array_key_exists('query', $parts)) {
+            parse_str($parts['query'], $query);
+            if(array_key_exists('exam', $query)){
+                $course_id = $query['exam'];
+                $user = Auth::user();
 
-        $exam = Exam::find($course_id);
+                $exam = Exam::find($course_id);
 
-        $newExam = $exam->replicate();
-        $newExam->name = $this->name;
-        $newExam->author = $user->id;
+                $newExam = $exam->replicate();
+                $newExam->name = $this->name;
+                $newExam->author = $user->id;
 
-        $newExam->push();
-         
-        foreach($exam->questions as $question)
-        {
+                $newExam->push();
+                 
+                foreach($exam->questions as $question)
+                {
 
-           $newQ = $question->replicateRow();
-           $newExam->questions()->attach($newQ->id, ['show_in_result' => $question->exams[0]->pivot['show_in_result'], 'latest_question' => $question->exams[0]->pivot['latest_question'], 'first_question' => $question->exams[0]->pivot['first_question']]);
+                   $newQ = $question->replicateRow();
+                   $newExam->questions()->attach($newQ->id, ['show_in_result' => $question->exams[0]->pivot['show_in_result'], 'latest_question' => $question->exams[0]->pivot['latest_question'], 'first_question' => $question->exams[0]->pivot['first_question']]);
 
+                }
+
+                $newExam->save();
+                session()->flash('message', 'Course Imported Successfully. To see this course go to Courses');
+            }
+        }
+        else{
+            session()->flash('error-message', 'This Url does not belong to any course');
         }
 
-        $newExam->save();
-
-
-       /* $q = Question::find($id);
-        $newQ = $q->replicateRow();
-        $this->course->questions()->attach($newQ->id, ['show_in_result' => $q->exams[0]->pivot['show_in_result'], 'latest_question' => $q->exams[0]->pivot['latest_question'], 'first_question' => $q->exams[0]->pivot['first_question']]);
-
-        $this->course->refresh();
-        $this->questions = $this->course->questions; 
-        foreach($this->questions as $q){
-            $q->first_question = $q->pivot['first_question'];
-            $q->latest_question = $q->pivot['latest_question'];
-            $q->show_in_result  =  $q->pivot['show_in_result'];    
-        } */
-
-        session()->flash('message', 'Course Imported Successfully. To see this course go to Courses'); 
+         
     }
 }
--- resources/views/livewire/trainer/import.blade.php
+++ resources/views/livewire/trainer/import.blade.php
@@ -4,6 +4,11 @@
           {{ session('message') }}
         </div>
      @endif
+     @if (session()->has('error-message'))
+        <div class="alert alert-danger" style="margin-top:30px;">
+          {{ session('error-message') }}
+        </div>
+     @endif
     <div class="content-wrapper">
         <div class="content ">
             <h2 class="mb-3">{{ __('Import Course') }}</h2>
@@ -13,11 +18,11 @@
                         <label for="url" class="form-label mt-3">{{__('Import File URL')}} <sup class="text-danger">*</sup></label>
                         <div class="input-group">
                             <input wire:model="url" type="text" class="form-control btn-fix-size"  :errors="$errors" autocomplete="off" />
-                                @error('url') <span class="error">{{ $message }}</span> @enderror
                             <div class="input-group-append">
                                 <button class="btn btn-orange btn-fix-size"> <i class="fas fa-cloud-upload-alt"></i> {{__('Cancel')}}</button>
                             </div>
                         </div>
+                        @error('url') <span class="error">{{ $message }}</span> @enderror
                     </div>
                     <div class="form-group">
                         <label for="name" class="form-label">{{__('Onder welke naam wil je deze course opslaan?')}} <sup class="text-danger">*</sup></sup></label>
